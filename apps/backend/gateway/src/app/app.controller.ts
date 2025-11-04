09e1f7742633db3d1ea1b07c6ec25df8a402d7b7
Itapit <itamardavid90@gmail.com>
2025-11-04 09:55:46 +0200

    refactor(gateway): Remove user communication from AppController

--- apps/backend/gateway/src/app/app.controller.ts
+++ apps/backend/gateway/src/app/app.controller.ts
@@ -1,23 +1,7 @@
-import { USER_PATTERNS, USER_SERVICE } from '@limbo/users-contracts';
-import { Controller, Get, Inject } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    @Inject(USER_SERVICE) private readonly client: ClientProxy // Inject the client
-  ) {}
-
-  @Get('check-comms')
-  async checkComms(): Promise<string> {
-    const payload = 'Hello from Gateway!';
-
-    // The send method returns an Observable. Use firstValueFrom to await the result.
-    const result = await firstValueFrom(this.client.send(USER_PATTERNS.PING, payload));
-
-    return `Microservice communication successful! Users Service replied: **${result}**`;
-  }
+  constructor(private readonly appService: AppService) {}
 }
