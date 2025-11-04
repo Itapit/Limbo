a0b6a7289e5618fda958a49aa78daee5f3d490d7
Itapit <itamardavid90@gmail.com>
2025-11-04 09:55:47 +0200

    feat(users-service): Add admin user creation handler

--- apps/backend/users-service/src/app/user/user.controller.ts
+++ apps/backend/users-service/src/app/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { UserDto } from '@limbo/common';
+import { AdminCreateUserPayload, USER_PATTERNS } from '@limbo/users-contracts';
+import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @MessagePattern(USER_PATTERNS.CREATE_USER)
+  @UsePipes(new ValidationPipe())
+  async adminInviteUser(@Payload() payload: AdminCreateUserPayload): Promise<UserDto> {
+    return this.userService.adminInviteUser(payload);
+  }
+}
