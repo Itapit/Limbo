c10416154ea87b2fbfc05b2b5924f19bc5f2e3a9
Itapit <itamardavid90@gmail.com>
2025-12-15 18:14:07 +0200

    Adds health check with prometheus and grafana

--- apps/backend/gateway/src/app.module.ts
+++ apps/backend/gateway/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { GroupsModule } from './groups/groups.module';
+import { HealthModule } from './health/health.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, GroupsModule],
+  imports: [HealthModule, UsersModule, AuthModule, GroupsModule],
   controllers: [],
   providers: [],
 })
